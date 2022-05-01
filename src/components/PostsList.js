c082548c251d8c40e5258633c70906934b7b2d13
Aleksey Volkov <avolkov@dtln.ru>
2022-05-01 14:03:21 +0300

    task 2 completed

---
+++ src/components/PostsList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {useNavigate} from 'react-router-dom';
+import Post from './Post';
+import PostModel from '../models/PostModel';
+import CommentForm from './CommentForm';
+
+const PostsList = ({posts}) => {
+    const navigate = useNavigate();
+    return (
+        <div className="posts">
+            {posts.map((o) => (
+                <div key={o.id} className="post-wrap">
+                    <Post post={o} clickable onClick={() => navigate(`/posts/${o.id}`)}/>
+                    <CommentForm/>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+PostsList.propTypes = {
+    posts: PropTypes.arrayOf(PropTypes.instanceOf(PostModel)).isRequired,
+};
+
+export default PostsList;
---
+++ src/pages/PostView.js
@@ -0,0 +1,75 @@
+import React, {useContext, useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
+import {Link, useNavigate} from 'react-router-dom';
+import PostsContext from '../contexts/PostsContext';
+import Post from '../components/Post';
+import NotFound from '../components/NotFound';
+
+const PostView = ({id}) => {
+    const {posts, deletePost, refresh} = useContext(PostsContext);
+    const [post, setPost] = useState(null);
+    const [deleting, setDeleting] = useState(false);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (posts) {
+            setPost(posts.find((o) => o.id === id));
+        }
+    }, [posts, id]);
+
+    const handleEdit = () => {
+        navigate('edit');
+    };
+
+    const handleDelete = () => {
+        setDeleting(true);
+        deletePost(id)
+            .then(() => {
+                setDeleting(false);
+                setError(null);
+                refresh();
+                navigate('/');
+            })
+            .catch((err) => {
+                setDeleting(false);
+                setError(err.message);
+            });
+    };
+
+    return posts ? (
+        post ? (
+            <div className="view">
+                <div className="header">
+                    <Link to="/" className="close">
+                        <span className="material-icons-outlined">close</span>
+                    </Link>
+                </div>
+                <Post post={post}/>
+                <div className="buttons">
+                    <button className="button" onClick={handleEdit} disabled={deleting}>
+                        Изменить
+                    </button>
+                    <button
+                        className="button button-delete"
+                        onClick={handleDelete}
+                        disabled={deleting}
+                    >
+                        {deleting ? 'Удаление...' : 'Удалить'}
+                    </button>
+                </div>
+                {error && <div>{error}</div>}
+            </div>
+        ) : (
+            <NotFound/>
+        )
+    ) : (
+        <div>Загрузка...</div>
+    );
+};
+
+PostView.propTypes = {
+    id: PropTypes.number.isRequired,
+};
+
+export default PostView;
